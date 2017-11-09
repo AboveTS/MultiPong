25007d454b0aff5d7f6752739c3e903719a4e03a
ThomasTheDankEngine666 <sasha.dmytryshyn@gmail.com>
2017-11-09 16:15:14 -0700

    FirstUnityProject
    Made by sasha *DISCLAIMER*
    100% profits go to me only

---
+++ MultiPong/Assets/Code/CreateServerCode.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class CreateServerCode : MonoBehaviour
+{
+    public void CreateGameButton()
+    {
+       
+        SceneManager.LoadScene(0);
+
+    }
+    public void BackGameButton()
+    {
+
+        SceneManager.LoadScene(0);
+
+    }
+
+}
---
+++ MultiPong/Assets/Code/JoinGameCode.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class JoinGameCode : MonoBehaviour
+{
+   
+    public void JoinGameButton()
+    {
+        
+            SceneManager.LoadScene(0);
+        
+    }
+    public void BackGameButton()
+    {
+
+        SceneManager.LoadScene(0);
+
+    }
+
+}
