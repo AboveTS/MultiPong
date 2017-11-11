10507b219adaf49f4c8f1713b7a516b8a765fc42
AboveTS <graydenjprice@gmail.com>
2017-11-11 13:07:02 -0700

    Maybe

---
+++ Assets/Code/MainMenuButtons.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenuButtons : MonoBehaviour
+{
+    public void JoinServerButton()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void HostServerButton()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+}
