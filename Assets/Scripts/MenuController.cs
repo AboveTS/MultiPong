8cd9f74dfac1185e4d77bccac54179db95ee4d84
Seth Hinz <sethhinz@me.com>
2017-11-25 16:08:27 -0700

    Working on menu overhaul, refactoring
    To fix scene change delays and code duplication

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void DisplayScreen(GameObject targetScreen) {
+		foreach (GameObject screen in GameObject.FindGameObjectsWithTag("Screen")) {
+			screen.SetActive(false);
+		}
+
+		targetScreen.SetActive(true);
+	}
+}
