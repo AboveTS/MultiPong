c379394fcda1777ae458b67909655c75534a3a9e
Seth Hinz <sethhinz@me.com>
2017-12-19 20:36:41 -0700

    Added basic spawning code
    places several paddles in a ring

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,14 +5,23 @@ using UnityEngine;
 public class GameController : MonoBehaviour {
 	public GameObject paddlePrefab;
 	public GameObject ballPrefab;
+	public int paddleDistance;
 
 	private GameObject playerPaddle;
 
 	void Start () {
-		
+		Spawn(10);
 	}
 
 	void Spawn (int playerCount) {
+		for(int i = 0; i < playerCount; i++) {
+			GameObject currentPaddle = Instantiate(paddlePrefab, transform);
+			float theta = i * Mathf.PI * 2 / playerCount;
+			currentPaddle.transform.Translate(Mathf.Cos(theta) * paddleDistance , Mathf.Sin(theta) * paddleDistance, 0);
+		}
+	}
+
+	void DestroyAll() {
 
 	}
 	
